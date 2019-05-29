f960fe4b94d9157777fa686cae810cfc9daa61b4
Boyan Atanasov <b.atanasov@icloud.com>
2019-05-29 21:45:26 +0100

    implemented sfq_remaining + casts on preprocessor defined types

--- static_fifo_queue.c
+++ static_fifo_queue.c
@@ -18,7 +18,9 @@ sfqueue_t sfq_create_queue(const void* storage, SFQ_WIDTH_TYPE storage_width, SF
 sfqueue_err_t sfq_check_queue(const sfqueue_t* queue)
 {
     sfqueue_err_t ret = SFQ_ERR_OK;
-    if((queue->storage == NULL) || (queue->max_length == 0U) || (queue->storage_width == 0U))
+    if((queue->storage == NULL)
+    || (queue->max_length == (SFQ_LENGTH_TYPE) 0U)
+    || (queue->storage_width == (SFQ_WIDTH_TYPE) 0U))
     {
         ret = SFQ_ERR_ARG;
     }
@@ -32,7 +34,7 @@ sfqueue_err_t sfq_enqueue(sfqueue_t* queue, void* source)
     {
         ret = SFQ_ERR_ARG;
     }
-    else if(queue->length >= queue->max_length)
+    else if(queue->length >= (SFQ_LENGTH_TYPE) queue->max_length)
     {
         ret = SFQ_ERR_FULL;
     }
@@ -40,7 +42,7 @@ sfqueue_err_t sfq_enqueue(sfqueue_t* queue, void* source)
     {
         uint8_t* target = &(((uint8_t*)queue->storage)[queue->tail * queue->storage_width]);
         SFQ_WIDTH_TYPE storage_idx;
-        for(storage_idx = 0U; storage_idx < queue->storage_width; storage_idx++)
+        for(storage_idx = (SFQ_WIDTH_TYPE) 0U; storage_idx < queue->storage_width; storage_idx++)
         {
             target[storage_idx] = ((uint8_t*)source)[storage_idx];
         }
@@ -48,7 +50,7 @@ sfqueue_err_t sfq_enqueue(sfqueue_t* queue, void* source)
         queue->tail++;
         if(queue->tail >= queue->max_length)
         {
-            queue->tail = 0U;
+            queue->tail = (SFQ_LENGTH_TYPE) 0U;
         }
     }
     return ret;
@@ -61,7 +63,7 @@ sfqueue_err_t sfq_dequeue(sfqueue_t* queue, void* destination)
     {
         ret = SFQ_ERR_ARG;
     }
-    else if(queue->length == 0U)
+    else if(queue->length == (SFQ_LENGTH_TYPE) 0U)
     {
         ret = SFQ_ERR_EMPTY;
     }
@@ -69,7 +71,7 @@ sfqueue_err_t sfq_dequeue(sfqueue_t* queue, void* destination)
     {
         uint8_t* source = &(((uint8_t*)queue->storage)[queue->head * queue->storage_width]);
         SFQ_WIDTH_TYPE storage_idx;
-        for(storage_idx = 0U; storage_idx < queue->storage_width; storage_idx++)
+        for(storage_idx = (SFQ_WIDTH_TYPE) 0U; storage_idx < queue->storage_width; storage_idx++)
         {
             ((uint8_t*)destination)[storage_idx] = source[storage_idx];
         }
@@ -77,8 +79,19 @@ sfqueue_err_t sfq_dequeue(sfqueue_t* queue, void* destination)
         queue->length--;
         if(queue->head >= queue->max_length)
         {
-            queue->head = 0U;
+            queue->head = (SFQ_LENGTH_TYPE) 0U;
         }
+    }
+    return ret;
+}
+
+SFQ_LENGTH_TYPE sfq_remaining(sfqueue_t* queue)
+{
+    SFQ_LENGTH_TYPE ret = (SFQ_LENGTH_TYPE) 0U;
+    sfqueue_err_t err = sfq_check_queue(queue);
+    if(err == SFQ_ERR_OK)
+    {
+        ret = queue->length;
     }
     return ret;
 }
