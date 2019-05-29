75bb7e5be48f8cb533287ea96b50d8fb1473c4f1
Boyan Atanasov <b.atanasov@icloud.com>
2019-05-29 19:17:33 +0100

    gitignore + project init :fire:

---
+++ static_fifo_queue.c
@@ -0,0 +1,7 @@
+#include "static_fifo_queue.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ static_fifo_queue.h
@@ -0,0 +1,6 @@
+#ifndef C_STATIC_FIFO_LIBRARY_H
+#define C_STATIC_FIFO_LIBRARY_H
+
+void hello(void);
+
+#endif
